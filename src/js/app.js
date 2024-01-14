969ddb0fa2fad657eb2de4e1852346a2affb93d2
Paul Solovey <p.v.solovey@ya.ru>
2024-01-14 20:24:39 +0300

    LocalStorage added

--- src/js/app.js
+++ src/js/app.js
@@ -2,29 +2,29 @@ import TaskManadger from './listener';
 
 // localStorage.clear();
 
-// window.addEventListener('beforeunload', () => {
-//   localStorage.clear();
-//   const column = document.querySelectorAll('.column');
-//   column.forEach((el) => {
-//     const columnName = el.id;
-//     const tasks = el.querySelector('.tasks');
-//     localStorage.setItem(columnName, tasks.outerHTML);
-//   });
-// });
+window.addEventListener('beforeunload', () => {
+  localStorage.clear();
+  const column = document.querySelectorAll('.column');
+  column.forEach((el) => {
+    const columnName = el.id;
+    const tasks = el.querySelector('.tasks');
+    localStorage.setItem(columnName, tasks.outerHTML);
+  });
+});
 
-// if (localStorage.todo) {
-//   for (let i = 0; i < localStorage.length; i++) {
-//     const column = localStorage.key(i);
-//     const taskList = localStorage.getItem(column);
+if (localStorage.todo || localStorage.inPtogress || localStorage.done) {
+  for (let i = 0; i < localStorage.length; i++) {
+    const column = localStorage.key(i);
+    const taskList = localStorage.getItem(column);
 
-//     const board = document.querySelector(`#${column}`);
-//     const btn = board.querySelector('.add-task-btn');
-//     board.querySelector('.tasks').remove();
-//     const div = document.createElement('div');
-//     div.innerHTML = taskList;
-//     board.insertBefore(div, btn);
-//   }
-// }
+    const board = document.querySelector(`#${column}`);
+    const btn = board.querySelector('.add-task-btn');
+    board.querySelector('.tasks').remove();
+    const div = document.createElement('div');
+    div.innerHTML = taskList;
+    board.insertBefore(div, btn);
+  }
+}
 
 const manager = new TaskManadger(document.querySelector('.container'));
 
